a403c868c448f4fcbe4c2275a01726b36b89c63a
Dan Kirvy Manongsong <dankirvymanongsong@gmail.com>
2025-09-21 12:00:32 +0800

    Updated Database Info

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12799461',
+    'password'	=> 'zTERh8bdP1',
+    'database'	=> 'sql12799461',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
